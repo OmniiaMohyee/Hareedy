9ddba5d5eb89020a2669dd88da518bb1f3dc8c3a
OmniiaMohyee <zicomohyee@yahoo.com>
2019-05-04 19:48:21 +0200

    upload and download added. ready to merge

---
+++ upload_download/client.py
@@ -0,0 +1,120 @@
+#Client
+
+import socket 
+import os
+import sys
+
+
+
+def download(s,client_id):
+
+	print("Enter the file name")
+	file_name = input()
+
+	if file_name != 'q':
+		s.send(bytes(str(client_id),'utf-8'))
+		s.send(bytes(file_name,'utf-8'))
+		print(file_name)
+		data = s.recv(1024).decode('utf-8')
+		print(data[:6])
+		if data[:6] == 'EXISTS':
+			file_size = float(data[6:])
+			print("File Exists " + str(file_size)+" bytes, download? [Y/N]")
+			message = input()
+
+			if message == 'Y':
+				s.send(bytes('OK','utf-8'))
+				f = open(file_name,'wb')
+				data = s.recv(1024)
+				totalrecv = len(data)
+				f.write(data)
+				tmp = 0
+				while totalrecv < file_size:
+					data = s.recv(1024)
+					totalrecv += len(data)
+					f.write(data)
+					percent = int((totalrecv/float(file_size))*100)
+					if percent != tmp:
+						print(str(percent)+"% Done.")
+						tmp = percent
+
+				print("ٍSUCCESS!")
+		else:
+			print("File does not exist!")
+	s.close()
+	return
+
+def upload(s,client_id):
+
+	print("Enter the file name")
+	file_name = input()
+
+	total_send = 0
+	if file_name != 'q':
+		if not(os.path.isfile(file_name)):
+			print("Error! : invalid File Name")
+		else:
+			
+			name_size_cid = file_name + '#'+str(os.path.getsize(file_name))+'#'+str(client_id)
+			s.send(bytes(name_size_cid,'utf-8'))
+
+			print("file name is "+ file_name)
+			with open(file_name,'rb') as f:
+				bytesToSend = f.read(1024)
+				s.send(bytesToSend)
+				while bytesToSend != "" and total_send <= os.path.getsize(file_name)  :
+					bytesToSend = f.read(1024)
+					total_send += 1024
+					s.send(bytesToSend)
+			f.close()
+	status = s.recv(1024).decode('utf-8')
+	print(status)
+	s.close()
+	return 
+def main(client_id):
+
+	print("hello, you're user with id" +str(client_id))
+
+	print("Hello, for file upload enter U and for file downlaod enter D")
+	
+
+	
+	choice = input()
+	host = '127.0.0.1'
+	port = 20000
+	data_node_port = 0 
+	s = socket.socket()
+	s.connect((host,port))
+	if choice == "U":
+		print("going to upload")
+		s.send(bytes("U",'utf-8'))
+		data_node_port = int(s.recv(1024).decode('utf-8'))
+
+
+		data_s = socket.socket()
+		data_s.connect((host,data_node_port))
+		upload(data_s,client_id)
+
+	elif choice == "D":
+		print("going to download")
+		s.send(bytes("D",'utf-8'))
+		data_node_ports = s.recv(3072).decode('utf-8').split('#')
+		print(data_node_ports)
+		print("choose a port to download from")
+		i = int(input())
+
+		while i>6 or i<1:
+			print("enter a valid port number")
+			i = int(input())
+
+		data_s = socket.socket()
+		data_s.connect((host,int(data_node_ports[i-1])))
+		download(data_s,client_id)
+	return 
+if __name__ == '__main__':
+	
+	client_id = int(sys.argv[1])
+	main(client_id)
+
+
+				
---
+++ upload_download/master_tracker.py
@@ -0,0 +1,268 @@
+import mysql.connector as mysql
+import zmq
+import sys
+import time
+import random
+import json
+from multiprocessing import Process
+import socket
+import threading
+
+data_node_sock = []
+
+
+def init_data_nodes_database():
+    db = mysql.connect(
+        host="localhost",
+        user="root",
+        passwd="hydragang"
+    )
+    cursor = db.cursor()
+    cursor.execute("CREATE DATABASE data_nodes")
+
+
+def init_data_nodes_tables():
+    db = mysql.connect(
+        host="localhost",
+        user="root",
+        passwd="hydragang",
+        database="data_nodes"
+    )
+
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE `file_table` (`user_id` INT NOT NULL, `file_name` VARCHAR(255) NOT NULL, `node_number` INT, `file_path` VARCHAR(255), `is_node_alive` BOOLEAN, `last_modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)")
+
+
+def init_system():
+    init_data_nodes_database()
+    init_data_nodes_tables()
+
+
+# This function subscribes to the data node's alive messages.
+def listen_to_alive_messages(address, port):
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://%s:%s" % (address, port))
+    socket.setsockopt_string(zmq.SUBSCRIBE, '')
+    print("Listening to ALIVE messages on %s:%s.." % (address, port))
+    db = mysql.connect(host="localhost", user="root", passwd="12345678", database="data_nodes")
+    cursor = db.cursor()
+    while True:
+        try:
+            received_message = socket.recv_string(flags=zmq.NOBLOCK)
+            node_id, message = received_message.split()
+            node_id = int(node_id)
+            print("Tracker: received %s on %s:%s" % (received_message, address, port))
+            cursor.execute("UPDATE file_table SET is_node_alive = TRUE WHERE node_number=%d" % node_id)
+            db.commit()
+            # print(cursor.rowcount)
+        except zmq.Again as e:
+            cursor.execute("UPDATE file_table SET is_node_alive = FALSE WHERE last_modified < NOW() - INTERVAL 1 MINUTE")
+            db.commit()
+            # print(cursor.rowcount)
+
+######===================== For Replication Part ================ #######
+
+def getInstanceCount(f_name):
+    db = mysql.connect(host="localhost", user="root", passwd="12345678", database="data_nodes")
+    cursor = db.cursor()
+    cursor.execute('SELECT count(*) FROM file_table WHERE file_name=%s', [f_name])
+    count =cursor.fetchall()
+    count = int(count[0][0])
+    print("file name is : ",f_name)
+    return count
+
+def getSourceMachine(f_name):  
+    db = mysql.connect(host="localhost", user="root", passwd="12345678", database="data_nodes")
+    cursor = db.cursor()
+    # cursor.execute("SELECT node_number  FROM file_table WHERE last_modified= (SELECT MIN(`last_modified`) FROM file_table WHERE file_name=%s)", [f_name])
+    cursor.execute("SELECT node_number  FROM file_table WHERE is_node_alive = TRUE AND file_name=%s", [f_name])
+    source =cursor.fetchall()
+    if(cursor.rowcount == 0):
+        source = -1
+        print("in if n get Source Machine")
+    else:
+        source = int(source[0][0])
+    print("outing safly from get Source Machine")
+    return source
+
+def selectMachineToCopyTo(f_name,offset):
+    db = mysql.connect(host="localhost", user="root", passwd="12345678", database="data_nodes")
+    cursor = db.cursor()
+    cursor.execute("SELECT node_number FROM file_table WHERE (node_number NOT IN(SELECT node_number FROM file_table WHERE file_name=%s)) AND is_node_alive = TRUE", [f_name])
+    selected =cursor.fetchall()
+    if (not selected or (len(selected)==1 and offset ==1) ):
+        print("no more available data nodes to replicate at")
+        return
+    else:
+        selected = selected[0+offset][0]
+    with open('config.json') as config_file:
+        data = json.load(config_file)
+        num_ports = data["num_data_node_ports"]
+        selected_id = selected-1 #just bcz it's not zero_indexed in database--may be changed later
+        node_addr = data["data_nodes"]["address"][selected_id]  
+        node_port = data["data_nodes"]["management_ports"][(selected_id)*num_ports+2]# dst recieve on 3rd port
+    return selected_id, node_addr, node_port
+
+def NotifyMachineDataTransfer(src, dst, f_name,client_id):
+    if (dst != None):
+        dst_id, dst_node_addr,dst_node_port = dst
+        with open('config.json') as config_file:
+            data = json.load(config_file)
+            num_ports = data["num_data_node_ports"]
+            src_node_addr = data["data_nodes"]["address"][src-1]
+            src_node_port_p1 = data["data_nodes"]["management_ports"][(src-1)*num_ports]
+            src_node_port_p2 = data["data_nodes"]["management_ports"][(src-1)*num_ports]
+            dst_node_port_p2 = data["data_nodes"]["management_ports"][(dst_id)*num_ports]
+
+        context = zmq.Context()
+        context2 = zmq.Context()
+
+        print("send msg to src on port %s" %src_node_port_p2)
+        socket = context.socket(zmq.PAIR)
+        socket.connect("tcp://%s:%s" % (src_node_addr, src_node_port_p2))#master connect with them on first port
+        socket.send_string("send %s %s %s %s" % (f_name, client_id, dst_node_addr, dst_node_port)) #3rd port
+        
+        print("send msg to dst on port %s" %dst_node_port_p2)
+        socket2 = context2.socket(zmq.PAIR)
+        socket2.connect("tcp://%s:%s" % (dst_node_addr, dst_node_port_p2))
+        socket2.send_string("recieve %s %s %s %s" % (f_name, client_id, src_node_addr, src_node_port_p1))
+        
+    return
+
+def replicate():
+    # master connect with 1st port of each data node
+    # data node send on 2nd port
+    # and recieve on 3rd port
+    db = mysql.connect(host="localhost", user="root", passwd="12345678", database="data_nodes")
+    cursor = db.cursor()
+    try:
+        try:
+            cursor.execute("SELECT file_name,user_id FROM file_table WHERE file_name IN (SELECT distinct file_name FROM file_table) group by file_name")
+        except mysql.Error as err:
+            print("Error in replication function infirst line: {}".format(err))
+            cursor.close()
+        files =cursor.fetchall()
+        for file in files: #for each distinct file instances:    
+            count = getInstanceCount(file[0]) 
+            if (count < 3):
+                print ("count = ", count)
+                print (file[0])
+                src = getSourceMachine(file[0])
+                if(src == -1):
+                    break
+                dst1 = selectMachineToCopyTo(file[0],0)
+                dst2 = selectMachineToCopyTo(file[0],1)
+                NotifyMachineDataTransfer(src, dst1, file[0],file[1])
+                NotifyMachineDataTransfer(src, dst2, file[0],file[1])
+    except mysql.Error as err:
+        print("Error in replication function: {}".format(err))
+        cursor.close()
+
+######===================== For Client Upload/Download Part ================ #######
+def wait_clients(port,data_node_sock):
+
+    s = socket.socket()
+    ip = '127.0.0.1'
+    s.bind((ip,20000))
+    s.listen(5)
+    
+    while True :
+        c,addr = s.accept()       
+        print("A Client with IP:<"+str(addr)+"> has a request.")
+        t = threading.Thread(target = get_client_request , args = ("saveThread",c,data_node_sock))
+        t.start()
+    s.close()
+ 
+def get_client_request(name,sock,data_node_sock):
+    print("got here! \n")
+    request_type = sock.recv(1024).decode('utf-8')
+    print("request_type is " + str(request_type))
+    print(data_node_sock)
+    print(len(data_node_sock))
+
+    db = mysql.connect(host="localhost", user="root", passwd="12345678", database="data_nodes")
+    cursor = db.cursor()
+    cursor.execute("SELECT node_number FROM file_table WHERE is_node_alive = TRUE;")
+    selected =cursor.fetchall()
+    print(selected)
+    #need to generate random 
+    if request_type == 'U':
+        i = random.randrange(0, 3, 6)
+        i = 3
+        sock.send(bytes(str(data_node_sock[i]),'utf-8'))
+    else:
+    	sock_to_send = str(data_node_sock[1])+'#'+str(data_node_sock[2])+'#'+str(data_node_sock[4])+'#'
+    	sock_to_send += str(data_node_sock[5])+'#'+str(data_node_sock[7])+'#'+str(data_node_sock[8])
+    	print(sock_to_send)
+    	sock.send(bytes(sock_to_send,'utf-8'))
+def add_file(s):
+	data_node_id,client_id,file_name = s.recv(2048).decode('utf-8').split('#')
+	print("adding file from master tracker "+data_node_id)
+	db = mysql.connect(host="localhost", user="root", passwd="12345678", database="data_nodes")
+	cursor = db.cursor()
+	cursor.execute("INSERT INTO file_table (user_id,node_number,file_name,file_path) VALUES ("+client_id+","+data_node_id+",'"+file_name+"','"+file_name+"');")
+	db.commit()
+	cursor.close()
+
+	print("file added into DB!")
+	s.send(bytes("SUCCESS!",'utf-8'))
+
+def file_logger(file_log_port):
+	s = socket.socket()
+	ip = '127.0.0.1'
+	s.bind((ip,file_log_port))
+	s.listen(5)
+
+	while True:
+		c,addr = s.accept()
+
+		t = threading.Thread(target = add_file,args =(c,))
+		t.start()
+	s.close()
+
+if __name__ == "__main__":
+    client_port = 20000
+    file_log_port = 20010
+    with open('config.json') as config_file:  # Should probably check this exists first.
+        data = json.load(config_file)
+        master_addr = data["master_trackers"]["address"]
+        ports = data["master_trackers"]["ports"]
+        data_node_sock = data["data_nodes"]["client_ports"]
+
+    for i in range(6):
+        data_node_sock[i] = int(data_node_sock[i])
+
+    db = mysql.connect(host="localhost", user="root", passwd="12345678", database="data_nodes")
+    cursor = db.cursor()
+    # cursor.execute("SELECT exists(select * from file_table where user_id = 4 and file_name ='bp.mp4');")
+    # print(cursor.fetchall()[0][0])
+    port = ports[int(sys.argv[1])]  # Should probably check the argument is given first.
+    print(ports)
+    active_listener0 = Process(
+        target=listen_to_alive_messages, args=(master_addr, ports[0],), daemon=True)
+    active_listener1 = Process(
+        target=listen_to_alive_messages, args=(master_addr, ports[1],), daemon=True)
+    active_listener2 = Process(
+        target=listen_to_alive_messages, args=(master_addr, ports[2],), daemon=True)
+    
+    client_listener = Process( 
+        target=wait_clients,args=(client_port,data_node_sock,),daemon=True)
+
+    #replicator = Process(
+       # target = replicate, args = (), daemon=True)
+    file_log = Process(
+   		target = file_logger, args=(file_log_port,), daemon = True)
+    active_listener0.start()
+    active_listener1.start()
+    active_listener2.start()
+    client_listener.start()
+
+   # replicator.start()
+    file_log.start()
+    while True:
+        # print("Master tracker rollin' yon way.")
+        time.sleep(0.5)
+
+# listen_to_alive_messages(9232)
+# init_system()
